4591ca4f17be27dafc9cd3548752da2617feb3f5
Shen Tian <biozinc@gmail.com>
2018-02-27 00:33:37 +0200

    Implementing a full ctor for WebAuth

--- src/re_auth0/core.cljs
+++ src/re_auth0/core.cljs
@@ -1,5 +1,6 @@
 (ns re-auth0.core
   (:require [cljsjs.auth0]
+            [clojure.walk :as walk]
             [re-frame.core :as re-frame]))
 
 (defonce web-auth-instance (atom nil))
@@ -8,10 +9,33 @@
   [js]
   (js->clj js :keywordize-keys true))
 
+(defn remove-nils-and-empty
+  "Take a map, and remove all nil or empty values"
+  [m]
+  (let [f (fn [[k v]] (when (not (or (nil? v)
+                                     (empty? v)))
+                        [k v]))]
+    (walk/postwalk (fn [x] (if (map? x) (into {} (map f x)) x)) m)))
+
+(defn web-auth
+  [{:keys [domain client-id redirect-uri scope
+           audience response-type response-mode]}]
+  (js/auth0.WebAuth. (clj->js
+                      (remove-nils-and-empty
+                       {:domain       domain
+                        :clientID     client-id
+                        :redirectUri  redirect-uri
+                        :scope        scope
+                        :audience     audience
+                        :responseType response-type
+                        :responseMode response-mode}))))
+
 (defn make-web-auth
   [client-id domain]
   (js/auth0.WebAuth. (clj->js {:clientID client-id
-                               :domain domain})))
+                               :domain domain}))
+  (web-auth {:domain domain
+             :client-id client-id}))
 
 (defn auth-results-cb
   [on-auth-result on-error]
@@ -64,3 +88,8 @@
 (re-frame/reg-fx
  ::web-auth
  web-auth-fx)
+
+(re-frame/reg-fx
+ ::init
+ (fn [options]
+   (reset! web-auth-instance (web-auth options))))
